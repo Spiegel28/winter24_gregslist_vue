41c212ad5fd959abfbe7eb585b750dca0106672b
Alex Spiegel <alexspiegel28@gmail.com>
2024-02-27 15:02:16 -0700

    Houses page and router

---
+++ src/pages/HousePage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="m-3">
+                    Houses
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+setup(){
+return{}
+}
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
